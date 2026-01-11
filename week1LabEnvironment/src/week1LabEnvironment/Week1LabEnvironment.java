bcc048acbf23e081c9b1fdcd4797ef4ff39a9029
brownj87 <167582678+brownj87@users.noreply.github.com>
2026-01-11 15:29:01 -0500

    Add files via upload

---
+++ week1LabEnvironment/src/week1LabEnvironment/Week1LabEnvironment.java
@@ -0,0 +1,12 @@
+package week1LabEnvironment;
+
+public class Week1LabEnvironment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println();
+		System.out.println("Congratulations on your first Java program");
+	}
+
+}
